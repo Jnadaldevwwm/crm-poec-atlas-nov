5d7287fb59894c4c71d61a81b4456498faac7db9
Julien Nadal-Larios <julien.devwwm79@gmail.com>
2021-12-21 16:25:59 +0100

    final stage

---
+++ src/app/shared/pipes/roles-tab.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { RolesTabPipe } from './roles-tab.pipe';
+
+describe('RolesTabPipe', () => {
+  it('create an instance', () => {
+    const pipe = new RolesTabPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,13 +7,14 @@ import { TemplatesModule } from '../templates/templates.module';
 import { BtnActionComponent } from './components/btn-action/btn-action.component';
 import { BtnLinkComponent } from './components/btn-link/btn-link.component';
 import { BtnRouteComponent } from './components/btn-route/btn-route.component';
+import { FilterComponent } from './components/filter/filter.component';
 import { FormSearchComponent } from './components/form-search/form-search.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { TabLightComponent } from './components/tab-light/tab-light.component';
 import { StateDirective } from './directives/state.directive';
+import { RolesTabPipe } from './pipes/roles-tab.pipe';
 import { RolesPipe } from './pipes/roles.pipe';
 import { TotalPipe } from './pipes/total.pipe';
-import { FilterComponent } from './components/filter/filter.component';
 
 
 
@@ -28,7 +29,8 @@ import { FilterComponent } from './components/filter/filter.component';
     StateDirective,
     RolesPipe,
     FormSearchComponent,
-    FilterComponent
+    FilterComponent,
+    RolesTabPipe
   ],
   imports: [
     CommonModule,
@@ -48,7 +50,8 @@ import { FilterComponent } from './components/filter/filter.component';
     StateDirective,
     RolesPipe,
     FormSearchComponent,
-    FilterComponent
+    FilterComponent,
+    RolesTabPipe
   ]
 })
 export class SharedModule { }
